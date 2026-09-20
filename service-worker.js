96be5397222277dc40750717e75057ab81426c40
cudnisvemir1992 <digitalbridge.space@gmail.com>
2026-09-20 09:59:59 +0200

    Scope service worker cache cleanup

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
       .then(keys => Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        keys.filter(key => key.startsWith('moj-ritam-') && key !== CACHE_NAME).map(key => caches.delete(key))
       ))
       .then(() => self.clients.claim())
   );
